abeee3610f2d68d18de7100d7ea310b20c2a92f2
maglisg <magyg16@gmail.com>
2022-08-20 21:21:58 -0300

    desafiate (listo)

--- js/login.js
+++ js/login.js
@@ -26,3 +26,14 @@ let ingreso = document.getElementById ("registrarse")
         comprobar();
     } )
 
+  
+    // Información del ¡Desafíate!
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+
+
