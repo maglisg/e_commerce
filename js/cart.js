96c08285ef72047cde7aaf720a0d5ee4094ef9dd
maglisg <magyg16@gmail.com>
2022-10-22 23:44:09 -0300

    funciones de calculo semana 6

--- js/cart.js
+++ js/cart.js
@@ -6,17 +6,79 @@ function ImpCarrito(elemento) {
   let htmlContentToAppend = "";
   for (i = 0; i < elemento.length; i++) {
     htmlContentToAppend += `
-    <tr>
+    <tr onclick="calcularSubtotal()">
     <th scope="row" style="max-width: 100px;"><img src="${elemento[i].image}" style="max-width:35%;"></th>
     <td><p>${elemento[i].name}</p></td>
     <td><p>${elemento[i].currency}${elemento[i].unitCost}</p></td>
-    <td><input type="number" class="subInput" value="${elemento[i].count}" oninput="subTotal('${elemento[i].currency}',${elemento[i].unitCost}, ${i})" min="1" style="max-width: 50px;"></td>
-    <td id="subtotal"><b><p>${elemento[i].currency}<span class="SubResult">${elemento[i].unitCost}</span></p></b></td>
-      </tr>
+    <td><input type="number" id="${elemento[i].id}1" value="${elemento[i].count}" oninput="subTotal('${elemento[i].currency}', ${elemento[i].unitCost}, ${elemento[i].id}1, ${elemento[i].id})" min="1" style="max-width: 50px;"></td>
+    <td ><b><p>USD <span class="SubResult" id="${elemento[i].id}">${subTotal1((toString(elemento[i].currency)), elemento[i].unitCost )}</span></p></b></td>
+    </tr>
       `;
   }
   document.getElementById("bodyTable").innerHTML += htmlContentToAppend;
 }
+
+let dolar = (innerHTML = "USD");
+function subTotal(moneda, precio, i, id) { 
+    cantidad = document.getElementById(i).value
+    if (moneda === dolar){
+      resultado = (cantidad*precio);
+  }
+   else if (moneda != dolar){
+    resultado = Math.round((precio/40)*cantidad) 
+  }
+    document.getElementById(id).innerHTML= `${resultado}`;
+
+};
+function subTotal1(moneda, precio) { 
+  if (moneda === dolar){
+    resultado = (1*precio);
+}
+ else if (moneda != dolar){
+  resultado = Math.round((precio/40)) 
+}
+  return resultado;
+
+};
+
+
+let comissionPercentage = 0;
+
+function calcularSubtotal(){
+var subtotalPrecio = 0;
+let total = 0;
+let celdasPrecio = document.getElementsByClassName("SubResult");
+for (let i=0; i<celdasPrecio.length; ++i){
+ subtotalPrecio += parseFloat(celdasPrecio[i].firstChild.data);
+}; 
+document.getElementById("subTotalPor").innerHTML= `USD ${subtotalPrecio}`;
+resultado = Math.round(comissionPercentage * subtotalPrecio);
+document.getElementById("porcenEnvio").innerHTML = `USD ${resultado}`;
+total = (resultado + subtotalPrecio);
+document.getElementById("Total").innerHTML = `<b>USD ${total}</b>`
+}
+
+
+let arrayCarrito = [];
+document.addEventListener("DOMContentLoaded", function () {
+  let arrayCarrito = JSON.parse(localStorage.getItem("Productos_para_el_carrito"));
+  ImpCarrito(arrayCarrito);
+});
+//Eventos según envío seleccionado
+document.getElementById("premium").addEventListener("change", function(){
+ comissionPercentage = 0.15;
+ calcularSubtotal()
+});
+
+document.getElementById("express").addEventListener("change", function(){
+  comissionPercentage = 0.07;
+  calcularSubtotal()
+});
+
+document.getElementById("standard").addEventListener("change", function(){
+  comissionPercentage = 0.05;
+  calcularSubtotal()
+});
 
 getJSONData(DATA).then(function (resultObj) {
   if (resultObj.status === "ok") {
@@ -24,24 +86,4 @@ getJSONData(DATA).then(function (resultObj) {
     ObjelemeArt = ObjCarrito.articles;
     ImpCarrito(ObjelemeArt);
   }
-});
-
-const inputSubTotal = document.getElementsByClassName("subInput");
-const spanResult = document.getElementsByClassName("SubResult");
-
-function subTotal(moneda, precio, i) { 
-    cantidad = inputSubTotal[i].value
-    if (moneda === 'USD'){
-      resultado = (cantidad*precio);
-  }
-   else if (moneda != 'USD'){
-    resultado = Math.round((precio/40)*cantidad) 
-  }
-    spanResult[i].innerHTML=resultado;
-}
-
-let arrayCarrito = [];
-document.addEventListener("DOMContentLoaded", function () {
-  let arrayCarrito = JSON.parse(localStorage.getItem("Productos_para_el_carrito"));
-  ImpCarrito(arrayCarrito);
 });
